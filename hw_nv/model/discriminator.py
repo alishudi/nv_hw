a427bcdca3fa9c254897d532d9d381209cf793d7
alishudi <alexlishudi@gmail.com>
2022-12-21 23:28:29 +0400

    debug

--- hw_nv/model/discriminator.py
+++ hw_nv/model/discriminator.py
@@ -11,6 +11,7 @@ class Discriminator(nn.Module):
 
 
     def forward(self, y_false, y_true):
+        print(f'true shape {y_true.shape}, false shape {y_false.shape}')
         mpd_f_preds, mpd_f_fmaps = self.mpd(y_false)
         mpd_t_preds, mpd_t_fmaps = self.mpd(y_true)
         msd_f_preds, msd_f_fmaps = self.msd(y_false)
