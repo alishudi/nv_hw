4d28e1147eb218623c7740fceebd65b021a0e679
alishudi <alexlishudi@gmail.com>
2022-12-21 20:04:12 +0400

    fix? imports

--- hw_nv/model/__init__.py
+++ hw_nv/model/__init__.py
@@ -1,7 +1,11 @@
 from hw_nv.model.generator import Generator
 from hw_nv.model.discriminator import Discriminator
+from hw_nv.model.mpd import MPD
+from hw_nv.model.msd import MSD
 
 __all__ = [
     "Generator",
-    "Discriminator"
+    "Discriminator",
+    "MPD",
+    "MSD"
 ]
