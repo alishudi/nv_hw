405b1e2a2f2e1fe27e99af9dbdb4927e70c663fa
alishudi <alexlishudi@gmail.com>
2022-12-21 22:07:15 +0400

    generator fix?

--- hw_nv/model/generator.py
+++ hw_nv/model/generator.py
@@ -72,7 +72,8 @@ class Generator(nn.Module):
                 in_channels=out_channels * 2,
                 out_channels=out_channels,
                 kernel_size=(k_u[l],1),
-                stride=k_u[l]//2
+                stride=k_u[l]//2,
+                padding='same'
             )))
             layers.append(MRF(out_channels, D_r, K_r))
         self.blocks = nn.Sequential(*layers)
