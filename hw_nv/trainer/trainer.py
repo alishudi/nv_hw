d8c621902287edc8887ed1b02935f58bd6000f7a
alishudi <alexlishudi@gmail.com>
2022-12-22 23:45:46 +0400

    fix?

--- hw_nv/trainer/trainer.py
+++ hw_nv/trainer/trainer.py
@@ -148,9 +148,8 @@ class Trainer(BaseTrainer):
         #training discriminator
         batch["true_mels"] = self.melspec(batch["true_wavs"]).squeeze(dim=1)
         batch["gen_wavs"] = self.model_gen(batch["true_mels"])
-        batch["gen_mels"] = self.melspec(batch["gen_wavs"])
-        print(f'true shape {batch["true_mels"].shape}, gen shape {batch["gen_mels"].shape}')
-        assert batch["true_mels"].shape == batch["gen_mels"].shape
+        batch["gen_mels"] = self.melspec(batch["gen_wavs"]).squeeze(dim=1)
+        assert batch["true_wavs"].shape == batch["gen_wavs"].shape
 
         self.optimizer_disc.zero_grad()
 
