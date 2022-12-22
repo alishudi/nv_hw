552a91a84b2043848f4b4d0f19fa1bcb6e699116
alishudi <alexlishudi@gmail.com>
2022-12-22 04:47:49 +0400

    logging fix?

--- hw_nv/trainer/trainer.py
+++ hw_nv/trainer/trainer.py
@@ -198,7 +198,7 @@ class Trainer(BaseTrainer):
             gen_wav = self.model_gen(mel).squeeze(0)
             self.writer.add_audio(f"audio_{i}", gen_wav, sample_rate=22050)
             mel = self.melspec(gen_wav)
-            image = PIL.Image.open(plot_spectrogram_to_buf(mel.detach().cpu().numpy().transpose(-1, -2)))
+            image = PIL.Image.open(plot_spectrogram_to_buf(mel.detach().cpu().numpy().squeeze(0).transpose(-1, -2)))
             self.writer.add_image(f'melspec_{i}', ToTensor()(image))
         return
 
